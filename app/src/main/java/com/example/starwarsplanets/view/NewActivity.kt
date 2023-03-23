59df74c6c6d221098a8dc0581fbf6743c0a14980
Drakepal <playonplay23@gmail.com>
2023-03-23 12:16:13 -0500

    finished part 4

---
+++ app/src/main/java/com/example/starwarsplanets/view/NewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.starwarsplanets.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.starwarsplanets.R
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
